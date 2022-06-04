850f9fec536073b5d99536fe7357da65a528fb15
Diego Barbo <diegobarbox@gmail.com>
2022-06-04 10:38:23 -0300

    card component

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')
-    return render(request, 'home.html', {'searchTerm':searchTerm})
-
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
 
 
 def about(request):
     return HttpResponse('<h1>About</h1>')
+
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email':email})
