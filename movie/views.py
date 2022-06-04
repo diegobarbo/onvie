4c4d84295db1c3355e6da86ddf0a65418dfe275d
Diego Barbo <diegobarbox@gmail.com>
2022-06-03 23:27:37 -0300

    adding a search form

---
+++ movie/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm':searchTerm})
+
+
+
+def about(request):
+    return HttpResponse('<h1>About</h1>')
